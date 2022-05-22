3887239102d18acb3131eb30be9aa192c92a9a3c
speauty <speauty@163.com>
2022-05-22 16:42:43 +0800

    P007: 采用premake生成vs2022项目文件

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 本仓库相关代码，仅供学习Hazel游戏引擎使用
 
-### 相关依赖库
+### 相关依赖
 
 #### 1. 日志 [spdlog](https://github.com/gabime/spdlog)
 
 采用 `git-submodule` 管理，执行指令 `git submodule add git@git.zhlh6.cn:gabime/spdlog.git Hazel/vendor/spdlog` 可安装该依赖。确实卡，采用了加速源才下载成功。
+
+#### 2. [Premake](https://github.com/premake/premake-core)
+
+项目构建工具。其中 `GenerateProject.bat` 中的vs2022，需要适配成自己本地的工具集。
 
 
 
---
+++ premake5.lua
@@ -0,0 +1,94 @@
+-- 工作空间名称
+workspace "Hazel"
+	-- 架构
+	architecture "x64"
+	-- 配置
+	configurations {"Debug", "Release", "Dist"}
+
+-- 自定义变量 输出目录
+outputdirs = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- 项目 Hazel
+project "Hazel"
+	-- 位置
+	location "Hazel"
+	-- 项目类型
+	kind "SharedLib"
+	-- 语言
+	language "C++"
+	-- 目标目录
+	targetdir ("bin/" .. outputdirs .. "/%{prj.name}")
+	-- 中间目录(.o)
+	objdir ("bin-int/" .. outputdirs .. "/%{prj.name}")
+	-- 包含文件 ** 递归搜索标记
+	files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+	-- 包含路径
+	includedirs {
+		-- 日志库 spdlog
+		"%{prj.name}/vendor/spdlog/include"
+	}
+	-- 过滤器 windows
+	filter "system:windows"
+		-- ?方言
+		cppdialect "C++17"
+		-- 运行时静态支持
+		staticruntime "On"
+		-- WinSDK版本 这个需要本地化设置 这里保持最新版本
+		systemversion "10.0"
+		-- 宏定义
+		defines {
+			"HZ_BUILD_DLL", "HZ_PLATFORM_WINDOWS", "_WINDLL"
+		}
+		-- 构建后置操作指令集
+		postbuildcommands {
+			-- 首次运行 这里会找不到相应文件 从而导致异常 再次运行即可
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdirs .. "/Sandbox")
+		}
+	-- 过滤器 Debug配置 仅适用于Debug
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+	-- 过滤器 Release配置
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		-- 启用优化
+		optimize "On"
+	-- 过滤器 Dist配置
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+
+	--[[filter {"system:windows", "configurations:Release"}
+		-- 构建选项 采用多线程
+		buildoptions "/MT"
+	--]]
+
+-- 项目 Sandbox
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	targetdir ("bin/" .. outputdirs .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdirs .. "/%{prj.name}")
+	files {"%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp"}
+	includedirs {
+		"Hazel/vendor/spdlog/include",
+		"Hazel/src"
+	}
+	-- 链接
+	links {"Hazel"}
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+		defines {"HZ_PLATFORM_WINDOWS"}
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+	
+		
