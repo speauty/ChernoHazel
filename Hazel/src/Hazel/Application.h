39850dce666e57f6a143e4d22a800e1ebd39fd09
speauty <speauty@163.com>
2022-07-03 13:50:41 +0800

    P11：抽象Window，接入GLFW；

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Core.h"
+#include "Events/Event.h"
+#include "Window.h"
 
 namespace Hazel
 {
@@ -11,6 +13,10 @@ namespace Hazel
 		Application();
 		virtual ~Application();
 		void Run();
+
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	Application* CreateApplication();
